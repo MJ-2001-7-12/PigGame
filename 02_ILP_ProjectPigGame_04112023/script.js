6e51568fdb915308ae8603f988350ca05bebb66d
Muhammed Suhail M K <muhammed.mk@experionglobal.local>
2023-11-04 15:13:54 +0530

    Feat: Function to roll dice, Generate random number, Display value at current element has been implemented.

--- 02_ILP_ProjectPigGame_04112023/script.js
+++ 02_ILP_ProjectPigGame_04112023/script.js
@@ -1,1 +1,27 @@
 'use strict';
+
+
+
+//Function to Roll Dice
+function rollDice(){
+
+
+    let dice = document.getElementById('dice');
+     dice.style.transform = "scaleX(-1)";
+    // Returns a random integer from 1 to 6:
+    let diceNumber = Math.floor(Math.random() * 6) + 1; ; 
+    console.log(diceNumber)
+
+    //Displaying dice
+    document.getElementById("dice").src = `dice-${[diceNumber]}.png`; 
+
+    //Upadting Current Number
+    document.getElementById("current--0").innerHTML = diceNumber;
+
+    // const player1ScoreElement = document.getElementById('score--0');
+    // let currTotal = Number(player1ScoreElement.textContent)
+    // player1ScoreElement.textContent = currTotal+ Number(diceNumber);
+
+}
+
+
