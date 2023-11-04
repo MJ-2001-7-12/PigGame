4471e7842c33ce65e268a629c59aa084120c7e95
Muhammed Suhail M K <muhammed.mk@experionglobal.local>
2023-11-04 18:10:29 +0530

    Feat: Function to switch users manually has been implemented!

--- 02_ILP_ProjectPigGame_04112023/script.js
+++ 02_ILP_ProjectPigGame_04112023/script.js
@@ -14,15 +14,13 @@ function setZero() {
     player1ScoreElement.textContent = currentScore;
   }
  updateScore();*/
-let player1Score=0,player2score=0;
+//let player1Score=0,player2score=0;
 
 
 //Function to Roll Dice
 function rollDice(){
 
 
-    let dice = document.getElementById('dice');
-     dice.style.transform = "scaleX(-1)";
     // Returns a random integer from 1 to 6:
     let diceNumber = Math.floor(Math.random() * 6) + 1; ; 
     console.log(diceNumber)
@@ -30,27 +28,58 @@ function rollDice(){
     //Displaying dice
     document.getElementById("dice").src = `dice-${[diceNumber]}.png`; 
 
-    //Upadting Current Number
-    document.getElementById("current--0").innerHTML = diceNumber;
 
-    const player1ScoreElement = document.getElementById('score--0');
-    let currTotal = Number(player1ScoreElement.textContent)
-    player1ScoreElement.textContent = currTotal+ Number(diceNumber);
+
+    let playerId;
+
+    const player1= document.getElementById("player--1")
+    const player0= document.getElementById("player--0")
+
+
+ if(player1.classList.contains('player--active')){
+     player1.classList.remove("player--active");
+     player0.classList.add("player--active");
+     playerId = 0;
+    
+ }
+ else {
+     player0.classList.remove("player--active");
+     player1.classList.add("player--active");
+    playerId=1;
+      
+ }
+
+    
+ //   let playerId = 1;
+    updateScore(playerId,diceNumber)
+
 
 }
 
 
-const changeBg=() => {
-const player1= document.getElementById("player1")
-const player0= document.getElementById("player0")
-if(player1.classList.contains('player--active')){
-    player1.classList.remove("player--active");
-    player0.classList.add("player--active");
-    return player0;
+
+function updateScore(playerId, diceNumber){
+
+    let currPlayer;
+    let playerTotalScore;
+
+    if(playerId==1){
+        currPlayer = 'current--1';
+        playerTotalScore = 'score--1'
+    }else{
+        currPlayer = 'current--0';
+        playerTotalScore = 'score--0'
+
+    }
+
+    //current score of player
+    document.getElementById(currPlayer).innerHTML = diceNumber;
+
+
+    // const player1ScoreElement = document.getElementById('score--0');
+    // let currTotal = Number(player1ScoreElement.textContent)
+    // player1ScoreElement.textContent = currTotal+ Number(diceNumber);
+
 }
-else {
-    player0.classList.remove("player--active");
-    player1.classList.add("player--active"); 
-    return player1;  
-}
-}
+
+
