9561c330eab1d3f3d9ff1d4ed74edd66c1851f34
Bivina M V <bivina.mv@experionglobal.local>
2023-11-04 16:58:07 +0530

    feat:- changed background color for active player

--- 02_ILP_ProjectPigGame_04112023/script.js
+++ 02_ILP_ProjectPigGame_04112023/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+let player1Score=0,player2score=0;
 
 const changeBg=() => {
 const player1= document.getElementById("player1")
@@ -7,10 +8,11 @@ const player0= document.getElementById("player0")
 if(player1.classList.contains('player--active')){
     player1.classList.remove("player--active");
     player0.classList.add("player--active");
+    return player0;
 }
 else {
     player0.classList.remove("player--active");
-    player1.classList.add("player--active");   
+    player1.classList.add("player--active"); 
+    return player1;  
 }
-
 }
