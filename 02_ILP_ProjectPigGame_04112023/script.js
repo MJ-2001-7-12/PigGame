98f511c2965986055ff261a71e1f9a48360017e1
Merin P Jose <115718619+MJ-2001-7-12@users.noreply.github.com>
2023-11-04 16:58:03 +0530

    Update script.js

--- 02_ILP_ProjectPigGame_04112023/script.js
+++ 02_ILP_ProjectPigGame_04112023/script.js
@@ -25,18 +25,3 @@ function rollDice(){
 
 }
 
-
-const changeBg=() => {
-const player1= document.getElementById("player1")
-const player0= document.getElementById("player0")
-if(player1.classList.contains('player--active')){
-    player1.classList.remove("player--active");
-    player0.classList.add("player--active");
-}
-else {
-    player0.classList.remove("player--active");
-    player1.classList.add("player--active");   
-}
-
-}
-
