8146ed9f3aea141a6fe43705d6ca24b79cd57b69
Geevarghese Manamel Varghese <geevarghese.varghese@experionglobal.local>
2023-11-04 14:54:04 +0530

    feat: Set score and current to zero on onload and on onclick of new game

--- 02_ILP_ProjectPigGame_04112023/script.js
+++ 02_ILP_ProjectPigGame_04112023/script.js
@@ -1,1 +1,9 @@
 'use strict';
+//onClick of New Game
+function setZero() {
+  //console.log('New Game Clicked');
+  document.getElementById('score--0').textContent = '0';
+  document.getElementById('score--1').textContent = '0';
+  document.getElementById('current--0').textContent = '0';
+  ocument.getElementById('current--1').textContent = '0';
+}
