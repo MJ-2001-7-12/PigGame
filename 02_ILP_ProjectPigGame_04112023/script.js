89c022e112457d3efac2e0abdc1bd91c5f34f4f4
Merin P Jose <merin.jose@experionglobal.local>
2023-11-04 13:08:35 +0530

    feat:- first sprint code uploaded

--- 02_ILP_ProjectPigGame_04112023/script.js
+++ 02_ILP_ProjectPigGame_04112023/script.js
@@ -1,1 +1,8 @@
 'use strict';
+function updateScore() {
+    const currentScore = rollDice();
+    const player1ScoreElement = document.getElementById('score--0');
+    player1ScoreElement.textContent = currentScore;
+  }
+
+  updateScore();
