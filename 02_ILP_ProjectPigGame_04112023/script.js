a5349fa3d4110738f75ba26075f09f842e9a98f5
Merin P Jose <merin.jose@experionglobal.local>
2023-11-04 16:52:59 +0530

    fix: merging main

--- 02_ILP_ProjectPigGame_04112023/script.js
+++ 02_ILP_ProjectPigGame_04112023/script.js
@@ -1,27 +1,14 @@
 'use strict';
+/*function updateScore() {
+    const currentScore = rollDice();
+    const player1ScoreElement = document.getElementById('score--0');
+    player1ScoreElement.textContent = currentScore;
+  }
 
+  updateScore();*/
 
+    const player1ScoreElement = document.getElementById('score--0');
+    let currTotal = Number(player1ScoreElement.textContent)
+    player1ScoreElement.textContent = currTotal+ Number(diceNumber);
 
-const changeBg=() => {
-const player1= document.getElementById("player1")
-const player0= document.getElementById("player0")
-if(player1.classList.contains('player--active')){
-    player1.classList.remove("player--active");
-    player0.classList.add("player--active");
-}
-else {
-    player0.classList.remove("player--active");
-    player1.classList.add("player--active");   
-}
-
-}
-
-
-
-// function updateScore() {
-//     const currentScore = rollDice();
-//     const player1ScoreElement = document.getElementById('score--0');
-//     player1ScoreElement.textContent = currentScore;
-//   }
-
-//   updateScore();
+    
