d1fa0d5e58d7e1b67e8b43886cffe05ece53e0f2
Muhammed Suhail M K <muhammed.mk@experionglobal.local>
2023-11-05 07:20:32 +0530

    Fix: Function to switch players has been fixed(When dice roll a '1'), getActivePlayer() function returns the ID of the current player.

--- 02_ILP_ProjectPigGame_04112023/script.js
+++ 02_ILP_ProjectPigGame_04112023/script.js
@@ -6,80 +6,106 @@ function setZero() {
   document.getElementById('score--0').textContent = '0';
   document.getElementById('score--1').textContent = '0';
   document.getElementById('current--0').textContent = '0';
-  ocument.getElementById('current--1').textContent = '0';
+  document.getElementById('current--1').textContent = '0';
 }
-/*function updateScore() {
-    const currentScore = rollDice();
-    const player1ScoreElement = document.getElementById('score--0');
-    player1ScoreElement.textContent = currentScore;
-  }
- updateScore();*/
-//let player1Score=0,player2score=0;
+
 
 
 //Function to Roll Dice
 function rollDice(){
 
-
     // Returns a random integer from 1 to 6:
-    let diceNumber = Math.floor(Math.random() * 6) + 1; ; 
-    console.log(diceNumber)
+    let diceNumber = Math.floor(Math.random() * 6) + 1; 
+    //console.log(diceNumber)
 
     //Displaying dice
     document.getElementById("dice").src = `dice-${[diceNumber]}.png`; 
 
+    let playerId= getActivePlayer();
 
+    if(diceNumber==1){
 
-    let playerId;
+       switchActivePlayer();
 
-    const player1= document.getElementById("player--1")
-    const player0= document.getElementById("player--0")
+    }else{
+        
+       updateScore(playerId,diceNumber);
 
-
- if(player1.classList.contains('player--active')){
-     player1.classList.remove("player--active");
-     player0.classList.add("player--active");
-     playerId = 0;
-    
- }
- else {
-     player0.classList.remove("player--active");
-     player1.classList.add("player--active");
-    playerId=1;
-      
- }
-
-    
- //   let playerId = 1;
-    updateScore(playerId,diceNumber)
-
+    }
 
 }
 
 
 
+
+//Function to update score
 function updateScore(playerId, diceNumber){
 
     let currPlayer;
     let playerTotalScore;
 
     if(playerId==1){
+
         currPlayer = 'current--1';
         playerTotalScore = 'score--1'
+
     }else{
+
         currPlayer = 'current--0';
         playerTotalScore = 'score--0'
 
     }
 
-    //current score of player
+    //Current score of player
     document.getElementById(currPlayer).innerHTML = diceNumber;
 
-
-    // const player1ScoreElement = document.getElementById('score--0');
-    // let currTotal = Number(player1ScoreElement.textContent)
-    // player1ScoreElement.textContent = currTotal+ Number(diceNumber);
+    //Updating total score
+    let playerScoreElement = document.getElementById(playerTotalScore);
+    let currTotal = Number(playerScoreElement.textContent);
+    playerScoreElement.textContent = currTotal+ Number(diceNumber);
 
 }
 
 
+//function to switch player
+function switchActivePlayer(){
+
+    let player0 = document.getElementById('player--0');
+    let player1 = document.getElementById('player--1');
+
+    if(player1.classList.contains('player--active')){
+
+        player1.classList.remove("player--active");
+        player0.classList.add("player--active");
+
+    }
+    else {
+
+        player0.classList.remove("player--active");
+        player1.classList.add("player--active");
+           
+    }
+
+}
+
+
+
+
+//Function to get current active player
+function getActivePlayer(){
+
+    let player0 = document.getElementById('player--0');
+    let player1 = document.getElementById('player--1');
+
+    if(player1.classList.contains('player--active')){
+
+        return 1;
+    
+    }
+    else {
+
+        return 0;
+         
+    }
+
+}
