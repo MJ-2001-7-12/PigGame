1647508e8ee79332cbb550144ac189a4898f093c
Muhammed Suhail M K <muhammed.mk@experionglobal.local>
2023-11-06 09:27:11 +0530

    Fix:- Total score set to to zero(When roll dice to a '1')

--- 02_ILP_ProjectPigGame_04112023/script.js
+++ 02_ILP_ProjectPigGame_04112023/script.js
@@ -2,52 +2,56 @@
 
 //onClick of New Game
 function setZero() {
-  //console.log('New Game Clicked');
-  document.getElementById('score--0').textContent = '0';
-  document.getElementById('score--1').textContent = '0';
-  document.getElementById('current--0').textContent = '0';
-  document.getElementById('current--1').textContent = '0';
+    //console.log('New Game Clicked');
+    document.getElementById('score--0').textContent = '0';
+    document.getElementById('score--1').textContent = '0';
+    document.getElementById('current--0').textContent = '0';
+    document.getElementById('current--1').textContent = '0';
 }
 
 
 
 //Function to Roll Dice
-function rollDice(){
+function rollDice() {
 
     // Returns a random integer from 1 to 6:
-    let diceNumber = Math.floor(Math.random() * 6) + 1; 
+    let diceNumber = Math.floor(Math.random() * 6) + 1;
     //console.log(diceNumber)
 
     //Displaying dice
-    document.getElementById("dice").src = `dice-${[diceNumber]}.png`; 
+    document.getElementById("dice").src = `dice-${[diceNumber]}.png`;
 
-    let playerId= getActivePlayer();
+    let playerId = getActivePlayer();
 
-    if(diceNumber==1){
+    if (diceNumber == 1) {
 
-       switchActivePlayer();
+        document.getElementById(`current--${playerId}`).textContent = '0';
+        document.getElementById(`score--${playerId}`).textContent = '0';
 
-    }else{
-        
-    updateScore(playerId,diceNumber);
-    
+
+        switchActivePlayer();
+
+    } else {
+
+        updateScore(playerId, diceNumber);
+
     }
 }
 
 
 
 //Function to update score
-function updateScore(playerId, diceNumber){
+function updateScore(playerId, diceNumber) {
 
     let currPlayer;
     let playerTotalScore;
 
-    if(playerId==1){
+    if (playerId == 1) {
 
         currPlayer = 'current--1';
         playerTotalScore = 'score--1'
 
-    }else{
+    } else {
 
         currPlayer = 'current--0';
         playerTotalScore = 'score--0'
@@ -65,15 +69,15 @@ function updateScore(playerId, diceNumber){
     let currTotal = Number(playerScoreElement.textContent);
 
     //Display winner!
-    if( currTotal+diceNumber >=20){
+    if (currTotal + diceNumber >= 100) {
 
         //console.log(playerId+1+" Won!")
-        alert("PLAYER "+Number(playerId+1)+" WON!");
+        alert("PLAYER " + Number(playerId + 1) + " WON!");
         setZero();
 
-    }else{
+    } else {
 
-        playerScoreElement.textContent = currTotal+ Number(diceNumber);
+        playerScoreElement.textContent = currTotal + Number(diceNumber);
 
     }
 
@@ -83,21 +87,21 @@ function updateScore(playerId, diceNumber){
 
 
 //function to switch player
-function switchActivePlayer(){
+function switchActivePlayer() {
 
     let player0 = document.getElementById('player--0');
     let player1 = document.getElementById('player--1');
 
-    if(player1.classList.contains('player--active')){
+    if (player1.classList.contains('player--active')) {
 
         player1.classList.remove("player--active");
         player0.classList.add("player--active");
 
-    }else {
+    } else {
 
         player0.classList.remove("player--active");
         player1.classList.add("player--active");
-           
+
     }
 
 }
@@ -106,20 +110,20 @@ function switchActivePlayer(){
 
 
 //Function to get current active player
-function getActivePlayer(){
+function getActivePlayer() {
 
     let player0 = document.getElementById('player--0');
     let player1 = document.getElementById('player--1');
 
-    if(player1.classList.contains('player--active')){
+    if (player1.classList.contains('player--active')) {
 
         return 1;
-    
+
     }
     else {
 
         return 0;
-         
+
     }
 
 }
